e7623c010e965dc5f383d6bf2772026842734f2e
Emin Umut Gerçek <eumutgercek@gmail.com>
2022-11-15 20:09:12 +0300

    Orm user imp (#3) (#5)
    * add /users/:id from db for user
    
    * add /users/:usercredentials - post
    
    * add /users/:id - delete
    
    * add /users/ - get
    
    * add /users/:id/:usercredentials - put
    
    * add .env
    
    * Remove dead code
    
    * Add error handling
    
    Co-authored-by: Emin Umut <umut.gercek@picussecurity.com>
    
    Co-authored-by: Öncüm K. Yılmaz <55596806+yilmazoncum@users.noreply.github.com>
    Co-authored-by: Emin Umut <umut.gercek@picussecurity.com>

--- api/main.go
+++ api/main.go
@@ -1,25 +1,40 @@
 package main
 
 import (
+	"os"
+
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+type UserHandler struct {
+	repo UserRepository
+	db   *gorm.DB
+}
 
 func main() {
 	e := echo.New()
-	// Initialize DB and and create repository
-	repo := &MockRepository{
-		users: []User{
-			{Name: "Baris", Id: "0"},
-			{Name: "Oncum", Id: "1"},
-			{Name: "Umut", Id: "2"},
-			{Name: "UmutCil", Id: "3"},
-			{Name: "UmutInce", Id: "4"},
-		},
+
+	err := godotenv.Load("../.env")
+
+	if err != nil {
+		panic("Error loading .env file")
 	}
-	h := UserHandler{repo: repo}
+
+	dsn := os.Getenv("DB_CONNECTION_STRING")
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	h := UserHandler{db: db}
 
 	e.GET("/users", h.List)
 	e.GET("/users/:id", h.Get)
 	e.DELETE("/users/:id", h.Delete)
+	e.POST("/users/:name/:surname/:mail", h.Add)
+	e.PUT("/users/:id/:name/:surname/:mail", h.Update)
 	e.Logger.Fatal(e.Start(":8080"))
 }
--- api/user.go
+++ api/user.go
@@ -1,64 +1,134 @@
 package main
 
 import (
-	"errors"
-	"github.com/labstack/echo/v4"
-	"golang.org/x/exp/slices"
+	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
 )
 
 type User struct {
-	Name string `json:"name"`
-	Id   string `json:"id"`
+	gorm.Model //has ID, CreatedAt, UpdatedAt, DeletedAt
+	Name       string
+	Surname    string
+	Mail       string
 }
 
 type UserRepository interface {
 	Get(id string) (*User, error)
 	Delete(id string) error
 	List() ([]User, error)
-}
-
-type UserHandler struct {
-	repo UserRepository
+	Add(name string, surname string, mail string) error
 }
 
 func (handler *UserHandler) Get(c echo.Context) error {
-	id := c.Param("id")
-	u, err := handler.repo.Get(id)
+	idStr := c.Param("id")
+	id, err := strconv.Atoi(idStr)
 
 	if err != nil {
+		return c.JSON(http.StatusBadRequest, fmt.Sprintf("%s is not number", idStr))
+	}
+
+	//u, err := handler.repo.Get(id)
+	handler.db.AutoMigrate(&User{})
+
+	var user User
+
+	result := handler.db.First(&user, id)
+
+	if result.Error != nil {
 		return c.JSON(http.StatusBadRequest, "DB error")
 	}
 
-	if u == nil {
+	if result == nil {
 		return c.JSON(http.StatusNotFound, "User not found")
 	}
 
-	return c.JSON(http.StatusOK, u)
+	return c.JSON(http.StatusOK, user)
 }
 
 func (handler *UserHandler) Delete(c echo.Context) error {
-	id := c.Param("id")
-	err := handler.repo.Delete(id)
+	idStr := c.Param("id")
+	id, err := strconv.Atoi(idStr)
 
 	if err != nil {
+		return c.JSON(http.StatusBadRequest, fmt.Sprintf("%s is not number", idStr))
+	}
+
+	handler.db.AutoMigrate(&User{})
+
+	result := handler.db.Delete(&User{}, id)
+
+	if result.Error != nil {
 		return c.JSON(http.StatusBadRequest, "DB error")
 	}
 
-	return c.JSON(http.StatusOK, "OK")
+	if result == nil {
+		return c.JSON(http.StatusNotFound, "User not found")
+	}
+
+	return c.JSON(http.StatusOK, "User successfully Deleted")
 }
 
 func (handler *UserHandler) List(c echo.Context) error {
-	us, err := handler.repo.List()
 
-	if err != nil {
+	var users []User
+
+	result := handler.db.Find(&users)
+	if result.Error != nil {
 		return c.JSON(http.StatusBadRequest, "DB error")
 	}
 
-	return c.JSON(http.StatusOK, us)
+	return c.JSON(http.StatusOK, users)
 }
 
-type MockRepository struct {
+func (handler *UserHandler) Add(c echo.Context) error {
+	name := c.Param("name")
+	surname := c.Param("surname")
+	mail := c.Param("mail")
+
+	handler.db.AutoMigrate(&User{})
+
+	var user = User{Name: name, Surname: surname, Mail: mail}
+
+	result := handler.db.Create(&user)
+
+	if result.Error != nil {
+		return c.JSON(http.StatusBadRequest, "DB error")
+	}
+
+	return c.JSON(http.StatusOK, "User successfully added")
+}
+
+func (handler *UserHandler) Update(c echo.Context) error {
+	idStr := c.Param("id")
+	id, _ := strconv.Atoi(idStr)
+	name := c.Param("name")
+	surname := c.Param("surname")
+	mail := c.Param("mail")
+
+	var user User
+
+	handler.db.AutoMigrate(&User{})
+
+	handler.db.First(&user, id)
+
+	user.Name = name
+	user.Surname = surname
+	user.Mail = mail
+
+	result := handler.db.Save(&user)
+
+	if result.Error != nil {
+		return c.JSON(http.StatusBadRequest, "DB error")
+	}
+
+	return c.JSON(http.StatusOK, "User successfully updated")
+}
+
+/* type MockRepository struct {
 	users []User
 }
 
@@ -85,4 +155,4 @@ func (m *MockRepository) Delete(id string) error {
 
 func (m *MockRepository) List() ([]User, error) {
 	return m.users, nil
-}
+} */
