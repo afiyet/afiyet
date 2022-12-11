f40b9aca11d0ad24aa778ae019992814be03f552
Mehmet Umut İNCEER <mumutinceer@gmail.com>
2022-12-11 16:08:42 +0300

    stack navigation -> tab navigation

--- ui/mobile/Main.js
+++ ui/mobile/Main.js
@@ -4,20 +4,53 @@ import MapScreen from './pages/MapScreen';
 import ScannerScreen from './pages/ScannerScreen';
 import CartScreen from './pages/CartScreen';
 import ProfileScreen from './pages/ProfileScreen';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const Stack = createNativeStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 function Main() {
     return(
       <>
-        <Stack.Navigator>
-            <Stack.Screen name='Home' component={HomeScreen}/>
-            <Stack.Screen name='Map' component={MapScreen}/>
-            <Stack.Screen name='Scanner' component={ScannerScreen}/>
-            <Stack.Screen name='Cart' component={CartScreen}/>
-            <Stack.Screen name='Profile' component={ProfileScreen}/>
-        </Stack.Navigator>
+        <Tab.Navigator 
+          initialRouteName='Home'
+          activeColor="#000000">
+            <Tab.Screen 
+              name='Home' 
+              component={HomeScreen}
+              options={{
+                tabBarLabel: 'Home',
+                tabBarIcon: ({color}) => (<MaterialCommunityIcons name="home" color={color} size={26} />),
+              }}/>
+            <Tab.Screen 
+              name='Map' 
+              component={MapScreen}
+              options={{
+                tabBarLabel: 'Map',
+                tabBarIcon: ({color}) => (<MaterialCommunityIcons name="map" color={color} size={26} />),
+              }}/>
+            <Tab.Screen 
+              name='Scanner' 
+              component={ScannerScreen}
+              options={{
+                tabBarLabel: 'Scanner',
+                tabBarIcon: ({color}) => (<MaterialCommunityIcons name="qrcode" color={color} size={26} />),
+              }}/>
+            <Tab.Screen 
+              name='Cart' 
+              component={CartScreen}
+              options={{
+                tabBarLabel: 'Cart',
+                tabBarIcon: ({color}) => (<MaterialCommunityIcons name="cart-variant" color={color} size={26} />),
+              }}/>
+            <Tab.Screen 
+              name='Profile' 
+              component={ProfileScreen}
+              options={{
+                tabBarLabel: 'Profile',
+                tabBarIcon: ({color}) => (<MaterialCommunityIcons name="account-circle" color={color} size={26} />),
+              }}/>
+        </Tab.Navigator>
       </>
     );
 }
