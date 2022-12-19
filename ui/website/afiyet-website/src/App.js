faf0625f2d8221037e69aae8b009381b3b8ac5fc
brsikinci <58369631+brsikinci@users.noreply.github.com>
2022-12-19 17:39:24 +0300

    Website initial commit

---
+++ ui/website/afiyet-website/src/App.js
@@ -0,0 +1,11 @@
+import RestaurantLogin from "./restaurant-login/RestaurantLogin";
+
+function App() {
+  return (
+    <div className="App">
+      <RestaurantLogin />
+    </div>
+  );
+}
+
+export default App;
