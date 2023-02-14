6e57572b83f18d15146b2bfc91364440ab01a3cf
Mehmet Umut İNCEER <mumutinceer@gmail.com>
2023-02-14 16:58:40 +0300

    marginTop

---
+++ ui/mobile/components/cart/BillingInfo.js
@@ -0,0 +1,10 @@
+import { FlatList, Text, View, StyleSheet, StatusBar, Image, Pressable } from 'react-native';
+import React from 'react';
+
+export default function BillingInfo() {
+  return (
+    <View>
+      <Text>BillingInfo</Text>
+    </View>
+  )
+}
--- ui/mobile/pages/CartScreen.js
+++ ui/mobile/pages/CartScreen.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import { Text, View, StyleSheet, Button, TouchableOpacity, ScrollView } from 'react-native';
+import BillingInfo from '../components/cart/BillingInfo';
 import OrderItem from '../components/cart/OrderItem';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 
 export default function CartScreen() {
     
+    const insets = useSafeAreaInsets();
 
-  return (
-    <View style={styles.container}>
-        <ScrollView>
-            <OrderItem />
-            <OrderItem />
-            <OrderItem />
-            <OrderItem />
-            <OrderItem />
-            <OrderItem />
-            <OrderItem />
-            <OrderItem />
-            <OrderItem />
-        </ScrollView>
-    </View>
-  );
+    return (
+        <View style={[styles.container, {marginTop: insets.top}]}>
+            <ScrollView>
+                <OrderItem />
+                <OrderItem />
+                <OrderItem />
+                <OrderItem />
+                <OrderItem />
+                <OrderItem />
+                <OrderItem />
+                <OrderItem />
+                <OrderItem />
+                <BillingInfo />
+            </ScrollView>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
