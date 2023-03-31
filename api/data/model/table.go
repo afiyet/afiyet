6aa8a785a68443c87be9640e6cd9b5ace92e87bc
Cratoinyan <umutciloglu06@gmail.com>
2023-03-31 10:32:08 +0300

    add unique constraint for name and restaurant_id combination for tables table

--- api/data/model/table.go
+++ api/data/model/table.go
@@ -11,8 +11,8 @@ type Table struct {
 	CreatedAt    time.Time      `json:"createdAt,omitempty"`
 	UpdatedAt    time.Time      `json:"updatedAt,omitempty"`
 	DeletedAt    gorm.DeletedAt `gorm:"index" json:"deletedAt,omitempty"`
-	Name         string         `json:"name"`
+	Name         string         `gorm:"index:uniqueresttable,unique" json:"name"`
 	Orders       []Order        `gorm:"-" json:"orders,omitempty"`
-	RestaurantId string         `json:"restaurantId"`
+	RestaurantId string         `gorm:"index:uniqueresttable,unique" json:"restaurantId"`
 	Restaurant   Restaurant     `gorm:"foreignKey:RestaurantId" json:"restaurant,omitempty"`
 }
