a8e032a7b1378a26532a07d35192a971ea636479
cratoinyan <umutciloglu06@gmail.com>
2023-04-12 22:50:27 +0300

    make restId name fields nonunique

--- api/data/model/table.go
+++ api/data/model/table.go
@@ -11,8 +11,8 @@ type Table struct {
 	CreatedAt    time.Time      `json:"createdAt,omitempty"`
 	UpdatedAt    time.Time      `json:"updatedAt,omitempty"`
 	DeletedAt    gorm.DeletedAt `gorm:"index" json:"deletedAt,omitempty"`
-	Name         string         `gorm:"index:uniqueresttable,unique" json:"name"`
+	Name         string         `json:"name"`
 	Orders       []Order        `gorm:"-" json:"orders,omitempty"`
-	RestaurantId string         `gorm:"index:uniqueresttable,unique" json:"restaurantId"`
+	RestaurantId string         `json:"restaurantId"`
 	Restaurant   Restaurant     `gorm:"foreignKey:RestaurantId" json:"restaurant,omitempty"`
 }
