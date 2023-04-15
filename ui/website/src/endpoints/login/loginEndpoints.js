c5dea4cc85d1544b90b43ddee2061fbfc8612913
Mehmet Umut İNCEER <mumutinceer@gmail.com>
2023-04-16 01:01:19 +0300

    web bugfix

--- ui/website/src/endpoints/login/loginEndpoints.js
+++ ui/website/src/endpoints/login/loginEndpoints.js
@@ -1,4 +1,4 @@
-import { axiosClient } from "../axiosClient";
+import axiosClient from "../axiosClient";
 
 export function login(payload) {
     return axiosClient.post("/restaurants/login", payload);
