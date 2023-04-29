0cc3749e6f21dbf29aa5cf873851ce87852f28df
Emin Umut Gercek <eumutgercek@gmail.com>
2023-04-29 21:43:44 +0300

    add missing

--- ui/website/src/restaurant/RestaurantMain.js
+++ ui/website/src/restaurant/RestaurantMain.js
@@ -80,7 +80,7 @@ const RestaurantMain = () => {
       picture: pictureBase64 || picture,
       latitude: latitude,
       longitude: longitude,
-      //campaignPicture: campaignPictureBase64 || picture
+      campaignPicture: campaignPictureBase64 || picture
     }
 
     updateRestaurantInfo(restaurantState.restaurantId, payload)
