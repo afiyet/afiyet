2990ffa657928eb51531ee75bf8e792743c6ee54
Emin Umut Gercek <eumutgercek@gmail.com>
2023-04-30 23:12:10 +0300

    Add email feature

--- api/service/aws.go
+++ api/service/aws.go
@@ -6,16 +6,22 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/sesv2"
+	sesv2types "github.com/aws/aws-sdk-go-v2/service/sesv2/types"
 	"io"
 	"time"
 )
 
-const maxRequestTime = 5 * time.Second
-const CloudFrontUrl = "https://d33virtamuit9v.cloudfront.net"
-const S3BucketName = "afiyet-media"
+const (
+	maxRequestTime   = 5 * time.Second
+	CloudFrontUrl    = "https://d33virtamuit9v.cloudfront.net"
+	S3BucketName     = "afiyet-media"
+	FromEmailAddress = "afiyetapp@gmail.com"
+)
 
 type AmazonService struct {
-	s3 *s3.Client
+	s3    *s3.Client
+	sesv2 *sesv2.Client
 }
 
 func NewAmazonService() (*AmazonService, error) {
@@ -24,10 +30,12 @@ func NewAmazonService() (*AmazonService, error) {
 		return nil, fmt.Errorf("aws-config default package error: %w", err)
 	}
 
-	svc := s3.NewFromConfig(cfg)
+	s3 := s3.NewFromConfig(cfg)
+	sv2 := sesv2.NewFromConfig(cfg)
 
 	return &AmazonService{
-		s3: svc,
+		s3:    s3,
+		sesv2: sv2,
 	}, nil
 }
 
@@ -39,6 +47,39 @@ func (a AmazonService) S3Upload(name string, reader io.Reader) error {
 		Bucket: aws.String(S3BucketName),
 		Key:    &name,
 		Body:   reader,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a AmazonService) SendEmail(to, subject, body, bodyPlainText string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), maxRequestTime)
+	defer cancel()
+
+	_, err := a.sesv2.SendEmail(ctx, &sesv2.SendEmailInput{
+		Content: &sesv2types.EmailContent{
+			Simple: &sesv2types.Message{
+				Body: &sesv2types.Body{
+					Html: &sesv2types.Content{
+						Data: &body,
+					},
+					Text: &sesv2types.Content{
+						Data: &bodyPlainText,
+					},
+				},
+				Subject: &sesv2types.Content{
+					Data: &subject,
+				},
+			},
+		},
+		Destination: &sesv2types.Destination{
+			ToAddresses: []string{to},
+		},
+		FromEmailAddress: aws.String(FromEmailAddress),
 	})
 
 	if err != nil {
