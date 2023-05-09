326a191f7207df7a11a738cc05def7b24e0ae445
Emin Umut Gercek <eumutgercek@gmail.com>
2023-05-09 21:32:47 +0300

    Add restriction for pass change
    - 1 time
    - max 12 hour

--- api/data/repo/password.go
+++ api/data/repo/password.go
@@ -1,9 +1,12 @@
 package repo
 
 import (
+	"errors"
+	"fmt"
 	"github.com/afiyet/afiytet/api/data/model"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
+	"time"
 )
 
 type PasswordRepository struct {
@@ -45,23 +48,30 @@ func (pr PasswordRepository) IsEmailValid(pe model.PasswordChange) (bool, error)
 }
 
 func (pr PasswordRepository) AddPasswordLog(pe model.PasswordChange, token string) error {
-	return pr.db.Exec("INSERT INTO password_changes (email, token, user_type) VALUES(?,?,?);",
-		pe.Email, token, pe.UserType).Error
+	return pr.db.Exec(`
+INSERT INTO password_changes (email, token, user_type, created_at)
+VALUES(?,?,?,?);`,
+		pe.Email, token, pe.UserType, time.Now()).Error
 }
 
 func (pr PasswordRepository) ValidateToken(pe model.PasswordTemp) (bool, error) {
-	var result string
-	err := pr.db.Raw("select 1 from password_changes pc where pc.token = ?", pe.Token).Scan(&result).Error
+	var result time.Time
+	err := pr.db.Raw(`
+select created_at from password_changes
+pc where pc.token = ?
+	and updated_at is null;
+`,
+		pe.Token).Scan(&result).Error
 
 	if err != nil {
 		return false, err
 	}
 
-	if result == "1" {
+	if result.Add(time.Hour * 12).Before(time.Now()) {
 		return true, nil
+	} else {
+		return false, errors.New("passwd more than 12 hours")
 	}
-
-	return false, nil
 }
 
 func (pr PasswordRepository) ChangePassword(pe model.PasswordTemp) error {
@@ -88,6 +98,16 @@ func (pr PasswordRepository) ChangePassword(pe model.PasswordTemp) error {
 		if err != nil {
 			return err
 		}
+	}
+
+	err = pr.db.Exec(`
+update password_changes
+set updated_at = ?
+where token =?
+`, time.Now(), pe.Token).Error
+
+	if err != nil {
+		return fmt.Errorf("update password's update time %#v", err)
 	}
 
 	return nil
